fe1dfe69054a174cacccd3b22d31f4a09194a97b
Jinliang <miku.cy@foxmail.com>
2023-12-07 13:32:57 +0800

    fix(uart): 修复了IDLE中断异常无限触发的bug
    现在中断标志位正确清除

--- Hardware/src/uart.c
+++ Hardware/src/uart.c
@@ -274,8 +274,21 @@ void DMA1_Channel5_IRQHandler(void) {
  */
 void USART1_IRQHandler(void) {
     /* Check for IDLE line interrupt */
-    if (USART_GetITStatus(USART1, USART_IT_IDLE)) {
-        USART_ClearITPendingBit(USART1, USART_IT_IDLE);        /* Clear IDLE line flag */
+    if (USART_GetITStatus(USART1, USART_IT_IDLE) == SET) {
+//        log_d("USART_IT_IDLE Interrupt");
+        /**
+        * @note
+        *   - PE (Parity error), FE (Framing error), NE (Noise error), ORE (OverRun
+        *     error) and IDLE (Idle line detected) pending bits are cleared by
+        *     software sequence: a read operation to USART_SR register
+        *     (USART_GetITStatus()) followed by a read operation to USART_DR register
+        *     (USART_ReceiveData()).
+        */
+        /* So let's comment it */
+//        USART_ClearITPendingBit(USART1, USART_IT_IDLE);
+        /* Then read the DR register */
+        USART1->DR;
+        /* Here the USART_IT_IDLE pending bit cleared */
         uart_rx_check();                                                       /* Check for data to process */
     }
 
